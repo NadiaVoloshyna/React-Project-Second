86a133cb06ab29f784af30271bcf3939cc8975f3
NadiaVoloshyna <dnipronadiya@gmail.com>
2020-06-29 16:39:41 +0300

    more work

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map((igKey) => {
     return (
     <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
     </li>);
     });
-return (
+        return(
 <Aux>
 <h3>Your order</h3>
 <p>A delicious burger with following ingredients:</p>
 <ul>
 {ingredientSummary}
 </ul>
-<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+<p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
 <p>Continue to checkout?</p>
-<Button clicked={props.purchaseCancelled}>CANCEL</Button>
-<Button clicked={props.purchaseContinued}>CONTINUE</Button>
+<Button clicked={this.props.purchaseCancelled}>CANCEL</Button>
+<Button clicked={this.props.purchaseContinued}>CONTINUE</Button>
 </Aux>
-);
-};
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className="Toolbar">
-        <div>MENU</div>
-        <Logo />
-        <nav>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className="LogoT">
+            <Logo />
+        </div>
+        
+        <nav className="DesktopOnly">
             <NavigationItems />
         </nav>
     </header>
